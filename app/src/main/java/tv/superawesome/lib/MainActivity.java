55610afd8d51bc8ca9032d3401a8c23dc25e1dce
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-02-02 12:26:23 +0000

    0.3.1 - Added SAImageUtil that will allow no dependencies on drawables

--- app/src/main/java/tv/superawesome/lib/MainActivity.java
+++ app/src/main/java/tv/superawesome/lib/MainActivity.java
@@ -1,13 +1,26 @@
 package tv.superawesome.lib;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import tv.superawesome.lib.sautils.SAImageUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView testImageView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testImageView = (ImageView) findViewById(R.id.TestImageView);
+        testImageView.setAlpha(0.5f);
+        Bitmap bitmap = SAImageUtils.gameWallBackground();
+        if (bitmap != null) {
+            testImageView.setImageBitmap(bitmap);
+        }
     }
 }
